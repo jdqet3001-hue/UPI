708bef8611d6709df77507336eeea59ee67ae268
jd804570 <jdqet3001@gmail.com>
2026-09-03 20:09:40 -0500

    release 1.0

---
+++ SETUP.py
@@ -0,0 +1,65 @@
+import shutil
+import os
+import time
+import sys
+
+
+
+def is_admin():
+    try:
+        # Check for Linux/macOS
+        if hasattr(os, 'getuid'):
+            return os.getuid() == 0
+        
+        # Check for Windows
+        import ctypes
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except Exception:
+        return False
+
+if is_admin():
+    print("starting setup")
+    time.sleep(2)
+else:
+    print("rerun the setup as admin")
+    time.sleep(5)
+    sys.exit(0)
+
+    
+
+
+
+path = input("enter your system for path isntallation\n 1.windows\n 2.linux\n 3.darwin/macOS\n 4.custom\n please enter the number of your chioce (1-4)\n")
+if path == "1":
+    path = "C:\\Windows\\System32"
+if path in("2", "3"):
+    path = "/local/usr/bin/"
+if path == "4":
+    path = input("enter the path of your system:\n")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+path_installer = os.path.join(script_dir, "path_installer.py")
+pathloc = os.path.join(script_dir, "pathloc.txt")
+
+shutil.copy(path_installer, path)
+
+
+fp = open(
+    'pathloc.txt', 'w')
+fp.write(path)
+fp.close()
+time.sleep(1)
+
+with open(pathloc, "r", encoding="utf-8") as file:
+    content = file.read()
+
+time.sleep(1)
+
+alsothistoolonpath = input("do you want to also add this tool to your path? (y/n)")
+
+if alsothistoolonpath in("y", "Y"):
+    shutil.copy(path_installer, path)
+if alsothistoolonpath in("n", "N"):
+    print("ok :(")
+    time.sleep(2)
+sys.exit(0)
---
+++ path_installer.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import time
+import sys
+
+
+def is_admin():
+    try:
+        # Check for Linux/macOS
+        if hasattr(os, 'getuid'):
+            return os.getuid() == 0
+        
+        # Check for Windows
+        import ctypes
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+    except Exception:
+        return False
+
+if is_admin():
+    print("starting setup")
+    time.sleep(2)
+else:
+    print("rerun the setup as admin")
+    time.sleep(5)
+    sys.exit(0)
+
+file_path = input("Please enter the path to the file you want to add to PATH: ")
+
+answer = input(
+    "Welcome to Path Installer, select your system\n 1. windows\n 2. linux\n 3. custom\n 4. darwin/macOS\n")
+
+
+
+if answer in ("custom", "3"):
+    manual_setup = input("Did you already run manualpath_SETUP.py? ")
+
+    if manual_setup == "no":
+        print("Please run manualpath_SETUP.py first to make custom install")
+    with open("pathloc.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+        time.sleep(1)
+        shutil.copy(file_path, content)
+
+if answer in ("windows", "1"):
+    shutil.copy(file_path, "C:\\Windows\\System32")
+if answer in ("linux", "2"):
+    shutil.copy(file_path, "/local/usr/bin/")
+if answer in ("darwin", "4"):
+    shutil.copy(file_path, "/usr/local/bin/")
+if answer not in ("darwin", "4", "linux", "2", "windows", "1"):
+    print("unspported file/system")
+    time.sleep(3)
